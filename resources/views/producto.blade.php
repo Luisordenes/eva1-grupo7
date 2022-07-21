f12407a19786bd6d5bb13fb98a23298059dbb3ed
Luisordenes <luis.sebastian.90@hotmail.com>
2022-07-21 00:39:36 -0400

    primer commit

---
+++ resources/views/producto.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.master')
+
+@section('content')
+        
+        
+        
+        <div class="container">  
+            
+            <h1>Ingrese producto</h1>
+            <form method="GET" class="form-group" id="formulario">
+                
+                <br>
+
+                <!-- Nombre --> 
+                <label>Nombre Producto:</label>
+                
+                    <div>         
+                        
+                        <input type="Text" name="nombreProducto" required>
+                        
+                    </div>
+                
+                <br>
+
+                <!-- Categoria --> 
+                <label>Seleccione categoria:</label>
+
+                    <div>
+                        <select name="categoria" required>
+                            <option>Seleccione categoria</option>
+                            <option value="Vestuario">Vestuario</option>
+                            <option value="Tecno">Tecnologia</option>
+                            <option value="Muebles">Muebles</option>
+                        </select>    
+                    </div>
+                
+                <br>
+
+                <!-- Sucursal -->
+                <label>Seleccione sucursal:</label>    
+                
+                    <div>
+
+                        <input type="radio" name="radioSucursal" id="radioSucursal" value="1" required/>Santiago
+                        <input type="radio" name="radioSucursal" id="radioSucursal" value="2" required/>Viña del mar
+                        <input type="radio" name="radioSucursal" id="radioSucursal" value="3" required/>Rancagua
+
+                    </div>
+
+                <br>
+
+                <!-- Descripción -->
+                <label>Descripción:</label>    
+
+                    <div>
+
+                    <input type="Text" name="descripcionProducto" required>
+
+                    </div>  
+
+
+                <br> 
+                 
+                <!-- Cantidad -->
+                <label>Cantidad</label>
+
+                    <div>
+                        <input type="Number" name="cantidad" required>
+                    </div> 
+                    
+                <br> 
+                 
+                <!-- Precio -->
+                <label>Precio:</label>
+
+                    <div>
+                        <input type="Number" name="precio" required>
+                    </div> 
+
+                <br>
+
+                <div>
+                     <button type="submit"> Guardar</button>
+                 </div>
+
+
+            </form>      
+        </div>
+
+
+
+@stop         
